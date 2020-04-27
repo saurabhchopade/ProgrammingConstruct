9cb9a05d9f72cf2761594585763bba3cac1567a1
saurabh chopade <saurabhchopade3250@gmail.com>
2020-04-27 16:18:43 +0530

    Revert "Removed empwage from arithmatic"
    This reverts commit 27fac990367f6d965591ff56c4e4e15071cad542.

---
+++ employeeWage.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -x
+
+#CONSTANTS
+IS_PART_TIME=2;
+IS_FULL_TIME=1;
+EMP_RATE_PER_HR=50;
+MAX_HOURS_IN_MONTH=20;
+NUM_WORKING_DAYS=10;
+
+# VARIABLES
+totalWorkingDays=0;
+totalEmpHours=0;
+totalWage=0;
+
+# Dictionary Declaration
+declare -A  dailyWage
+
+	function getWorkingHours() {
+
+		case $1 in
+         $IS_FULL_TIME)
+            empHours=8;
+            ;;
+         $IS_PART_TIME)
+            empHours=4;
+            ;;
+         *)
+            empHours=0;
+            ;;
+      esac
+  		echo $empHours
+	}
+
+# To calculate  works Hours Daily
+
+	function calcWorkHours {
+
+		echo	$(( $1 * $EMP_RATE_PER_HR ));
+	}
+
+	while  [[ $totalWorkingDays -le $NUM_WORKING_DAYS ]] && [[ $totalEmpHours -lt $MAX_HOURS_IN_MONTH ]]
+	do
+
+		((totalWorkingDays++));
+		randomCheck=$(( RANDOM%3 ));
+      workHours="$( getWorkingHours $randomCheck )";
+		totalEmpHours=$(( $totalEmpHours + $workHours ));
+		dailyWage["DAY" "$totalWorkingDays"]="$( calcWorkHours $workHours)";
+
+	done
+
+echo "Days" ${!dailyWage[@]};
+echo "DailyWage=" ${dailyWage[@]};
+totalWage=$(( $totalEmpHours * $EMP_RATE_PER_HR ));
