f65ffd9fb95f52338745614fcd45c743ed3a3dba
saurabh chopade <saurabhchopade3250@gmail.com>
2020-04-27 13:53:00 +0530

    Total wage along with daily wage Feature

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x 
+#!/bin/bash -x
 
 #CONSTANTS
 IS_PART_TIME=2;
@@ -10,21 +10,32 @@ NUM_WORKING_DAYS=10;
 # VARIABLES
 totalWorkingDays=0;
 totalEmpHours=0;
+totalWage=0;
+
+# Array Declaration
+declare -a dailyWage
 
 	function getWorkingHours() {
-		case $1 in
 
-      	   $IS_FULL_TIME)
-         	   empHours=8;
-            	;;
-         	$IS_PART_TIME)
-            	empHours=4;
-            	;;
-         	*)
-            	empHours=0;
-            	;;
-		esac
+		case $1 in
+         $IS_FULL_TIME)
+            empHours=8;
+            ;;
+         $IS_PART_TIME)
+            empHours=4;
+            ;;
+         *)
+            empHours=0;
+            ;;
+      esac
   		echo $empHours
+	}
+
+# To calculate  works Hours Daily
+
+	function calcWorkHours {
+		
+		echo	$(( $1 * $EMP_RATE_PER_HR ));
 	}
 
 	while  [[ $totalWorkingDays -le $NUM_WORKING_DAYS ]] && [[ $totalEmpHours -lt $MAX_HOURS_IN_MONTH ]]
@@ -33,6 +44,10 @@ totalEmpHours=0;
 		((totalWorkingDays++));
 		randomCheck=$(( RANDOM%3 ));
       workHours="$( getWorkingHours $randomCheck )";
-		totalEmpHours=$(( $totalEmpHours + $workHours ));
-
+		totalEmpHours=$(( $totalEmpHours + $workHours ))
+		dailyWage[$totalWorkingDays]="$( calcWorkHours $workHours)";
+		
 	done
+
+totalWage=$(( $totalEmpHours * $EMP_RATE_PER_HR ));
+echo "DailyWage=" ${dailyWage[@]};
