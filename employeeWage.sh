43d444c540a437bea5facd1dbe732f5e207b6ee7
saurabh chopade <saurabhchopade3250@gmail.com>
2020-04-27 12:33:43 +0530

    Daily Wage feature Added

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,16 +1,21 @@
 #!/bin/bash -x
 
 isPresent=1;
-
 randomCheck=$((RANDOM%2));
 
 	if [[ $randomCheck -eq 1 ]]
 	then
 
-		echo "Employee is Present ";
-
+		echo "Employee is Present";
+		empRatePerHr=50;
+		empHours=8;
+		salary=$(($empRatePerHr*empHours));
+		
 	else
 
-		echo "Employee is Absent ";
+		echo "Employee is Absent";
+		salary=0;
 
 	fi
+
+	echo "Sallary=" $salary;
