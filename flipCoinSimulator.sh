0566df46ada6be91787d0180dec127a5fdeb5e28
saurabh chopade <saurabhchopade3250@gmail.com>
2020-04-28 01:19:31 +0530

    Who Won 21 time Feature

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,35 +1,37 @@
 #!/bin/bash -x
 #HERE 50% POSSIBLITIES OF GETTING NUMBER LESS THAN 5 OR GREATER THAN 5
-flip=$(( RANDOM %10 ));
 count1=0;
 count2=0;
-
-	if [ $flip -ge 5 ]
-   then
-		echo "Coin is Fliipping in the Air"; 
-		sleep 3;
-		echo "Yeahhh Heads";
-	else
-		echo "Coin is Fliipping in the Air";
-		sleep 3;
-		echo " Yeahhh Tails";
-	fi
-
-	for ((  flipCoin=0 ; flipCoin<=10 ;flipCoin++ ))
+	for ((  flipCoin=0 ; flipCoin<=50 ;flipCoin++ ))
 	do
-
 	flip=$(( RANDOM %10 ));
 		if [ $flip -ge 5 ]
    	then
 			echo "Coin is Fliipping in the Air"; 
 			sleep 1;
 			echo "Yeahhh Heads";
+			#sleep 1;
 			((count1++));
+
+			if [[ $count1 -eq 21 ]]
+			then
+    		 let  ans=$(( $count1 - $count2 ));
+				echo "Head Wins by" $ans;
+				exit 1;
+			fi;
+
 		else
 			echo "Coin is Fliipping in the Air";
 			sleep 1;
 			echo " Yeahhh Tails";
+			#sleep 1;
 			((count2++))
+			if [[ $count2 -eq 21 ]]
+         then
+			let	ans=$(( $count2 - $count1 ));
+            echo "Tails win  by" $ans;
+            exit 1;
+         fi;
 		fi
 	done
 
